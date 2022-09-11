f730ae8b2f200df5d4512671083863856c94acef
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-11 02:38:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
  * main - Entry point
  *   
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-/*This code prints a text using puts function*/
 	puts ("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
