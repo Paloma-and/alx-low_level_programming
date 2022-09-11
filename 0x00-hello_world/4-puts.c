475cba215ae5aef7126cc2eef0d09b6192b49f8c
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-11 02:24:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
 /*This code prints a text using puts function*/
 int main(void)
 {
+/*This code prints a text using puts function*/
 	puts ("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
