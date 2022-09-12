b30b62478ea898177b9109ae0e4fcdd3d408eb34
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-12 14:56:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Prints out the last part of a quote in the standard error.
  *
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 11);
 			return (1);
 }
