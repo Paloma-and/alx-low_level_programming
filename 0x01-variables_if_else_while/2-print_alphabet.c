e7b2f395994ac825c98cef104c1b67482e644481
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-12 17:25:02 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 'a';
+	for(x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	}
+		return (0);
+}
