e4bed01367913b0ae6698abf938f3156963cdbd4
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-12 18:15:16 -0700

    numb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	char space = '\n';
+
+	for (x = 0; x < 10 ; x++)
+	{
+	printf("%d", x);
+	}
+	putchar(space);
+		return (0);
+}
