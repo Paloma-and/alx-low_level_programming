5deba1bce350b1d826b077c142808fec4703522f
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 02:41:36 -0700

    grd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char p ='_';
+	char p1 = 'p';
+	char u = 'u';
+	char t = 't';
+	char c = 'c';
+	char h = 'h';
+	char a = 'a';
+	char r = 'r';
+	char space = '\n';
+
+	_putchar(p);
+	_putchar(p1);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar(space);
+		return (0);
+}
