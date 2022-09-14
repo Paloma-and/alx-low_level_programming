de42c594f7844ff596eb0ac02a5f6a09484a9a71
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 03:50:08 -0700

    sdf

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i;
+	char j = 'z';
+	char space = '\n';
+	
+	for (i = 'a'; i <= j; i++)
+	{
+	_putchar(i);
+	}
+	_putchar(space);
+		return ;
+}
