3358386a360bf4e7fc5778d7e41724dd4cdec81e
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 18:22:53 -0700

    sdfg

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - return the lower case caracter
+ *
+ * Returns: 0 or 1.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
