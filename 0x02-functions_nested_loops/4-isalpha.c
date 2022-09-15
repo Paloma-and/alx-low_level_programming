4c6682b69bbb598ab693d08659e8a68677afd733
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 19:07:13 -0700

    sdfg

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,12 @@
  *
  * @c: the character to evaluate
  *
- * Return: returns 1 if @c is an uppercase or a lowercase character, 0 otherwise.
+ * Return: returns 1 if @c is an uppercase or a lowercase character,
+ * 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <=90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
