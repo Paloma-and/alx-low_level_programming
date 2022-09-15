eb138726efca638915f7109f46676a1226deef9f
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 19:24:01 -0700

    gse

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - function
+ *
+ * @n: number being tested
+ *
+ * Return: 1 if @n is greater than 0, 0 if zero
+ * -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	return (-1);
+	}
+}
