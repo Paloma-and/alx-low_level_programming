1c37adf3cf19253382e11207b72445250be0239d
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 19:32:33 -0700

    gse

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_sign - function
  *
@@ -10,14 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
+	 _putchar('+');
+		return (1);	
 	}
 	else if (n < 0)
 	{
+	_putchar('-');
 		return (-1);
 	}
 	else
 	{
-	return (0);
+	_putchar('0');
+		return (0);
 	}
 }
