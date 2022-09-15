63675a8ed6ab050541de801ebe85c4c13db86f44
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-14 20:34:53 -0700

    edgrfhtgj

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 /**
+ * add - function
  *
+ *@a,@b: two integer
  *
- *
- *
- *
+ *Return: return res, sum of the two number
  */
 int add(int a, int b)
 {
 	int res;
 
-	res = a + b ;
+	res = a + b;
 		return (res);
 }
