86dfda563164dc52bfbcbf452c6354e3b98fa5d5
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-16 20:15:31 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+	char space = '\n';
+
+	for  (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar(space);
+		return;
+}
