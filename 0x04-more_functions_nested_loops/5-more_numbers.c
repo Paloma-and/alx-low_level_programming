727218477f9b34b5e8189498b6201c8358df9d17
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-17 02:41:51 -0700

    as

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	char space = '\n';
+
+	for (i = 0; i <= 10; i++)
+	{
+	for (j = 0; j <= 14; j++)
+	{
+	_putchar(j);
+	}
+	_putchar(space);
+	}
+	_putchar(space);
+		return;
+}
