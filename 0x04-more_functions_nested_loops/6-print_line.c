358da823f6ecfb8a678b2fd0699b9f2f22ac7968
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-19 04:58:31 -0700

    tyuj

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,20 @@
-#include "main.h" 
- /** 
+#include"main.h" 
+
+/** 
  * print_line - function 
  * 
  * @n: parameter 
  * 
  * Return: return value 
  */ 
- void print_line(int n) 
- { 
-         if (n > 0) 
-         { 
-                 while (n--) 
-                         _putchar(95); 
-         } 
-         _putchar(10); 
- }
+ void print_line(int n) 
+{ 
+	if (n > 0) 
+	{ 
+		while (n--) 
+		{
+		_putchar(95);
+		}
+	}	
+	_putchar(10); 
+}
