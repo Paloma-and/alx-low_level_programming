d589bfc6cbae31ecdcd5d8993554d311b16d51d3
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-22 06:15:07 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function printing a string
+ *
+ * @str: parameter
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	char c;
+
+	c = *str;
+	_putchar(c);
+		return;
+}
