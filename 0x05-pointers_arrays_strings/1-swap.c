e73cf23d4fbad7ea8409480e71018f619984789b
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-22 05:21:48 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int -  function
+ *
+ * @a: parameter
+ *
+ * @b: parameter
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+		return;
+}
