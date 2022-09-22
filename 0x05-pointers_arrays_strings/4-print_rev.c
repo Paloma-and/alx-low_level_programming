9ebc6cd8da3f4facd8e3b794d03f53753d8f6540
Paloma-and <paloma.ngokonna04@gmail.com>
2022-09-22 15:10:26 -0700

    inv

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ *
+ * @s: parameter
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	i++;
+		while (i > 0)
+		{
+		_putchar(s[--i]);
+		}
+	_putchar(10);
+	}
+		return;
+}
