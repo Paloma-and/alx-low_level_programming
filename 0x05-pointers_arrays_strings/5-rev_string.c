d5e026a69681abdae95925080e77782574b604b3
Paloma-and <68959921+Paloma-and@users.noreply.github.com>
2022-09-22 23:28:33 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * rev_string - function
- *
- * @s: pointer of parameter
- */
+* rev_string - function
+*
+* @s: pointer of parameter
+*/
 void rev_string(char *s)
 {
 	char temp;
@@ -13,14 +13,14 @@ void rev_string(char *s)
 
 	while (s[length] != '\0')
 		length++;
-		e = length - 1;
-		length /= 2;
-		while (length--)
-		{
+	e = length - 1;
+	length /= 2;
+	while (length--)
+	{
 		temp = s[b];
 		s[b] = s[e];
 		s[e] = temp;
 		b++;
 		e--;
-		}
+	}
 }
